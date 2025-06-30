a06a1fdcbce5b63be9044e0364058c84f8d95f88
kiashko777 <kiashko777@gmail.com>
2025-06-30 13:55:43 +0400

    feature:index and create views create
    views for jobs listings and create page are created

---
+++ resources/views/jobs/index.php
@@ -0,0 +1,19 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Job Listings</title>
+</head>
+<body>
+<h1>Available Jobs</h1>
+<ul>
+    <li>Web Developer</li>
+    <li>Database Admin</li>
+    <li>Software Engineer</li>
+    <li>System Analyst</li>
+</ul>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -7,5 +7,9 @@ Route::get('/', function () {
 });
 
 Route::get('/jobs', function () {
-    return "Avaiable Jobs";
+    return view('jobs.index');
 })->name('jobs');
+
+Route::get('/jobs/create', function () {
+    return view('jobs.create');
+})->name('jobs.create');
