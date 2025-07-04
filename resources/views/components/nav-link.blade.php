976b8833c914b4885350024f890a7b69480d6c63
kiashko777 <kiashko777@gmail.com>
2025-07-04 10:46:52 +0400

    feature:nav and button link components create
    nav link and button link components are created and styled

---
+++ resources/views/components/nav-link.blade.php
@@ -0,0 +1,9 @@
+@props(['url' => '/', 'active' => false, 'icon' => null])
+
+<a href="{{$url}}" class="block px-4 py-2 hover:bg-blue-700 {{$active ? 'text-yellow-500 font-bold' : ''}}">
+    @if($icon)
+        <i class="fa fa-{{$icon}}" mr-1"></i>
+    @endif
+    {{$slot}}
+</a>
+
