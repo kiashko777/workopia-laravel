76eaf50713860b2b393ea14be85c398350d274fa
kiashko777 <kiashko777@gmail.com>
2025-08-16 22:14:44 +0400

    feat:send mail
    mail sending is implemented using mailtrap

--- app/Http/Controllers/ApplicantController.php
+++ app/Http/Controllers/ApplicantController.php
@@ -4,8 +4,11 @@ namespace App\Http\Controllers;
 
 use App\Models\Applicant;
 use App\Models\Job;
+use App\Mail\JobApplied;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Mail;
+
 
 class ApplicantController extends Controller
 {
@@ -45,6 +48,9 @@ class ApplicantController extends Controller
         $application->job_id = $job->id;
         $application->user_id = auth()->id();
         $application->save();
+
+        //Send email to job owner
+        Mail::to($job->user->email)->send(new JobApplied($application, $job));
 
         return redirect()->back()->with('success', 'Application submitted successfully!');
     }
